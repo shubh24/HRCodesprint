84dfc69438b8d78529a1091a74288a945586a5ff
shubh24 <shubhankar24@gmail.com>
2016-09-01 12:21:30 +0530

    to hell with unsubscribe -- 0.42!

--- explore.R
+++ explore.R
@@ -4,7 +4,7 @@ test = read.csv("test_dataset.csv", stringsAsFactors = TRUE)
 train$train = 1
 test$train = 0
 
-train$unsubscribe_time[is.na(train$unsubscribe_time)] = -1
+#train$unsubscribe_time[is.na(train$unsubscribe_time)] = -1
 
 test$click_time = -1
 test$clicked = "false"
@@ -14,24 +14,24 @@ test$unsubscribe_time = -1
 test$unsubscribed = "false"
 
 df = rbind(train, test)
-agg_df = aggregate(unsubscribed ~ user_id, data = df, FUN = function(x) length(unique(x)))
-agg_df$unsubscribed[agg_df$unsubscribed == 1] = "false"
-agg_df$unsubscribed[agg_df$unsubscribed == 2] = "true"
-agg_df$unsubscribed = as.factor(agg_df$unsubscribed)
-df = merge(df, agg_df, by = "user_id", all.x = TRUE)
-df$unsubscribed.x = NULL
-names(df)[names(df) == "unsubscribed.y"] = "unsubscribed"
+#agg_df = aggregate(unsubscribed ~ user_id, data = df, FUN = function(x) length(unique(x)))
+#agg_df$unsubscribed[agg_df$unsubscribed == 1] = "false"
+#agg_df$unsubscribed[agg_df$unsubscribed == 2] = "true"
+#agg_df$unsubscribed = as.factor(agg_df$unsubscribed)
+#df = merge(df, agg_df, by = "user_id", all.x = TRUE)
+#df$unsubscribed.x = NULL
+#names(df)[names(df) == "unsubscribed.y"] = "unsubscribed"
 
-agg_df = aggregate(unsubscribe_time ~ user_id, data = df, FUN = function(x) max(x))
-df = merge(df, agg_df, by = "user_id", all.x = TRUE)
-df$unsubscribe_time.x = NULL
-names(df)[names(df) == "unsubscribe_time.y"] = "unsubscribe_time"
+#agg_df = aggregate(unsubscribe_time ~ user_id, data = df, FUN = function(x) max(x))
+#df = merge(df, agg_df, by = "user_id", all.x = TRUE)
+#df$unsubscribe_time.x = NULL
+#names(df)[names(df) == "unsubscribe_time.y"] = "unsubscribe_time"
 
 df$time_since_creation = df$sent_time - df$hacker_created_at
 df$time_since_online = df$sent_time - df$last_online
 
-df$time_since_unsubscribe[df$unsubscribe_time > 0] = df$sent_time[df$unsubscribe_time > 0] - df$unsubscribe_time[df$unsubscribe_time > 0]
-df$time_since_unsubscribe[df$unsubscribe_time <= 0] = -1 #doubtful -- figure out!
+#df$time_since_unsubscribe[df$unsubscribe_time > 0] = df$sent_time[df$unsubscribe_time > 0] - df$unsubscribe_time[df$unsubscribe_time > 0]
+#df$time_since_unsubscribe[df$unsubscribe_time <= 0] = -1 #doubtful -- figure out!
 
 #Removing all time-related factors
 df$sent_time = NULL
